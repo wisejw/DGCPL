bc846f7606aae681471eb92682d8f16a2c0906c2
wisejw <74333173+wisejw@users.noreply.github.com>
2025-01-21 16:39:31 +0800

    Update model.py

--- model.py
+++ model.py
@@ -17,16 +17,17 @@ class CPL(nn.Module):
         self.adj_out = adj_out
         self.adj_in = adj_in
 
-        '''HGCN'''
+        '''Concept-Resource HyperGraph: CRHG'''
         self.hgnn = HGNN(in_ch=in_channels, n_hid=out_channels1, n_class=out_channels2, dropout_rate=self.dropout_rate)
 
-        '''DGCN'''
+        '''Learning Behavior Graph: LBG'''
+        # Two different GCN layers for processing out-degree and in-degree graphs
         self.gcn1 = GCN(nfeat=in_channels, nhid=out_channels1, nclass=int(out_channels2))
         self.gcn2 = GCN(nfeat=in_channels, nhid=out_channels1, nclass=int(out_channels2))
         self.w1 = nn.Linear(out_channels2, out_channels2)
         self.w2 = nn.Linear(out_channels2, out_channels2)
 
-        '''kd'''
+        '''Gate Knowledge Distillation: GKD'''
         self.w3 = nn.Linear(out_channels2, out_channels2)
         self.w4 = nn.Linear(out_channels2, out_channels2)
 
@@ -35,19 +36,20 @@ class CPL(nn.Module):
         self.siameseNet2 = SiameseNet(out_channels2)
         self.siameseNet3 = SiameseNet(out_channels2)
 
+        # Sigmoid activation for output probabilities
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, c1, c2):
-        '''RelationGraph: HGCN'''
+        '''Concept-Resource HyperGraph: CRHG'''
         X_H = self.hgnn(self.feature_matrix, self.G)
 
-        '''TransitionGraph: DGCN'''
+        '''Learning Behavior Graph: LBG'''
         X_out = self.gcn1(self.feature_matrix, self.adj_out)
         X_in = self.gcn2(self.feature_matrix, self.adj_in)
         # X_N = torch.cat([X_out, X_in], -1)
         X_N = F.relu(self.w1(X_out) + self.w2(X_in))
 
-        '''KD'''
+        '''Gate Knowledge Distillation: GKD'''
         theta = self.sigmoid(self.w3(X_H) + self.w4(X_N))
         out_h = theta * X_H
         out_n = (1 - theta) * X_N
