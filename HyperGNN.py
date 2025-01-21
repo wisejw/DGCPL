8e014633562265c5975d15656cccb0e9749c5791
wisejw <74333173+wisejw@users.noreply.github.com>
2025-01-21 16:26:17 +0800

    Update HyperGNN.py

--- HyperGNN.py
+++ HyperGNN.py
@@ -6,7 +6,7 @@ from layers import HGNN_conv
 class HGNN(nn.Module):
     def __init__(self, in_ch, n_hid, n_class, dropout_rate=0.5):
         super(HGNN, self).__init__()
-        # Define graph convolution layers
+        # Define hypergraph convolution layers
         self.hgc1 = HGNN_conv(in_ch, n_hid)
         self.hgc2 = HGNN_conv(n_hid, n_class)
         # self.hgc3 = HGNN_conv(n_class, n_class)
@@ -20,9 +20,10 @@ class HGNN(nn.Module):
         self.dropout = nn.Dropout(dropout_rate)
 
     def forward(self, x, G):
-        # First graph convolution + ReLU activation + Fully connected layer + Dropout
+        # First hypergraph convolution + ReLU activation + Fully connected layer + Dropout
         x1 = self.dropout(F.relu(self.hgc1(x, G)) + self.linear1(x))
-        # Second graph convolution + ReLU activation + Fully connected layer + Dropout
+        # Second hypergraph convolution + ReLU activation + Fully connected layer + Dropout
         x2 = self.dropout(F.relu(self.hgc2(x1, G)) + self.linear2(x1))
+        # If a third hypergraph convolution layer is needed, apply similarly
         # x3 = self.dropout(F.relu(self.hgc2(x2, G)) + self.linear2(x2))
         return x2
